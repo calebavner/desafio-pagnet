7a4d7915482fb3f19e2c455af55434b7170fee50
calebavner <avner.trabalho@hotmail.com>
2024-01-04 20:19:35 -0300

    Criação do projeto

---
+++ backend-cnab/src/main/java/backendcnab/BackendCnabApplication.java
@@ -0,0 +1,13 @@
+package backendcnab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendCnabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendCnabApplication.class, args);
+	}
+
+}
