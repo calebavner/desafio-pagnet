282a1783770280ab71faeb737b80893e0462521c
calebavner <avner.trabalho@hotmail.com>
2024-01-04 21:43:20 -0300

    Processamento de arquivo CNAB com Spring Batch

---
+++ backend-cnab/src/main/java/backendcnab/model/TransacaoCNAB.java
@@ -0,0 +1,15 @@
+package backendcnab.model;
+
+import java.math.BigDecimal;
+
+public record TransacaoCNAB(
+  Integer tipo,
+  String data,
+  BigDecimal valor,
+  Long cpf,
+  String cartao,
+  String hora,
+  String donoDaLoja,
+  String nomeDaLoja
+) {
+}
