643d9a5cba7cf771934d497f731ea35d2f476f8d
calebavner <avner.trabalho@hotmail.com>
2024-01-14 10:51:17 -0300

    Implementado anotação de CrossOrigin no controller de transaçãoes

--- backend-cnab/src/main/java/backendcnab/controller/TransacaoController.java
+++ backend-cnab/src/main/java/backendcnab/controller/TransacaoController.java
@@ -3,6 +3,7 @@ package backendcnab.controller;
 import backendcnab.model.Transacao;
 import backendcnab.model.TransacaoReport;
 import backendcnab.service.TransacaoService;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,6 +21,7 @@ public class TransacaoController {
     }
 
     @GetMapping
+    @CrossOrigin(origins = {"http://localhost:9090"})
     List<TransacaoReport> listAll(){
         return service.totaisTransacoesPorNomeDaLoja();
     }
